01a92e5a7e6f073d2d15496868af9bd66c68b5cc
Valeron <Lerok.vasil@outlook.com>
2021-09-19 00:16:05 +0500

    add homework-2

--- first-project/src/components/Calculator.vue
+++ first-project/src/components/Calculator.vue
@@ -1,18 +1,55 @@
 <template>
   <div>
     <div class="display">
-      <input v-model.number="op1" />
-      <input v-model.number="op2" />
+      <input
+        type="number"
+        v-model.number="op1"
+        class="input"
+        v.el:inputOp1
+        @focus="checkedOp = 'op1'"
+      />
+      <input
+        type="number"
+        v-model.number="op2"
+        class="input"
+        v.el:inputOp2
+        @focus="checkedOp = 'op2'"
+      />
+      <span>= {{ result }}</span>
     </div>
-    <div class="keyboard">
-      <button @click="calculate('+')" class="btn">+</button>
-      <button @click="calculate('-')" class="btn">-</button>
-      <button @click="calculate('/')" class="btn">/</button>
-      <button @click="calculate('*')" class="btn">*</button>
-      <button @click="calculate('x')" class="btn">x <sup>y</sup></button>
-      <button @click="calculate('//')" class="btn">/ нацело</button>
-      <p class="error">{{ error }}</p>
-      <p>={{ result }}</p>
+    <div class="operands">
+      <button
+        v-for="(operand, idx) in operands"
+        :key="idx"
+        @click="calculate(operand)"
+        class="btn"
+      >
+        {{ operand }}
+      </button>
+    </div>
+    <p class="error">{{ error }}</p>
+    <label>
+      <input type="checkbox" v-model="checked" />
+      Отобразить экранную клавиатуру
+    </label>
+    <div class="keyboard" v-show="checked">
+      <button
+        v-for="(number, idx) in keyboard"
+        :key="idx"
+        v-html="number"
+        @click="setValue"
+        class="btn"
+      ></button>
+      <div>
+        <label>
+          <input type="radio" name="operand" value="op1" v-model="checkedOp" />
+          Первый операнд
+        </label>
+        <label>
+          <input type="radio" name="operand" value="op2" v-model="checkedOp" />
+          Второй операнд
+        </label>
+      </div>
     </div>
   </div>
 </template>
@@ -24,8 +61,13 @@ export default {
     return {
       op1: 0,
       op2: 0,
+      checkedOp: "",
       result: 0,
       error: "",
+      show: false,
+      operands: ["+", "-", "/", "*", "x^y", "%"],
+      keyboard: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "&larr;"],
+      checked: false,
     };
   },
   methods: {
@@ -43,9 +85,9 @@ export default {
           break;
         case "*":
           return this.getMulti();
-        case "x":
+        case "x^y":
           return this.getPow();
-        case "//":
+        case "%":
           if (this.validate()) {
             return this.getIntDiv();
           }
@@ -76,6 +118,31 @@ export default {
         return false;
       } else return true;
     },
+    focusOperand(e) {
+      if (e.target.value === "op1") {
+        this.$els.inputOp1.focus();
+      }
+    },
+    setValue(e) {
+      switch (this.checkedOp) {
+        case "op1":
+          {
+            if (e.target.textContent === "←") {
+              const str = this.op1.toString();
+              this.op1 = str.slice(0, str.length - 1);
+            } else this.op1 = +(this.op1 + e.target.textContent);
+          }
+          break;
+        case "op2":
+          {
+            if (e.target.textContent === "←") {
+              const str = this.op2.toString();
+              this.op2 = str.slice(0, str.length - 1);
+            } else this.op2 = +(this.op2 + e.target.textContent);
+          }
+          break;
+      }
+    },
   },
 };
 </script>
@@ -83,11 +150,15 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .btn {
+  width: 50px;
   height: 40px;
   margin: 10px;
   padding: 5px;
 }
 .error {
   color: red;
+}
+.input {
+  margin: 5px;
 }
 </style>
